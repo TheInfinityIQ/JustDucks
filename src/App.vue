2401d324339f0b9c616c964f90acf8a3f851a99d
Dawson K <46154365+TheInfinityIQ@users.noreply.github.com>
2023-05-22 16:43:33 -0500

    configured fonts for use with tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -5,12 +5,8 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
   <div class="container mx-auto bg-gray-200 rounded-xl shadow border p-8 m-10">
-     <p class="text-3xl text-gray-700 font-bold mb-5">
-       Welcome!
-     </p>
-     <p class="text-gray-500 text-lg">
-       Vue and Tailwind CSS in action
-     </p>
+    <div class="font-playfair text-48">Hello, World!</div>
+    <div class="font-source text-16">Hello, World!</div>
   </div>
 </template>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
 /** @type {import('tailwindcss').Config} */
+
+
 module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
+    fontFamily: {
+      'playfair': ['Playfair Display', 'serif'],
+      'source': ['Source Sans Pro', 'sans-serif']
+    },
+    fontSize: {
+      '48': '48px',
+      '16': '16px'
+    },
     extend: {},
   },
   plugins: [],
